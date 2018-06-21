ef5c60124306ace1205446b26c72967de624f948
sunghoon9@naver.com <root@bob.obenef.com>
2018-06-21 14:07:59 +0900

    Updated file : block--block--1.tpl.php

--- sites/all/themes/custom/gjmbc/templates/block/block--block--1.tpl.php
+++ sites/all/themes/custom/gjmbc/templates/block/block--block--1.tpl.php
@@ -44,13 +44,45 @@
  * @ingroup themeable
  */
 ?>
-<div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
-  <?php print render($title_prefix); ?>
-  <?php if ($block->subject): ?>
-  <h2<?php print $title_attributes; ?>><?php print $block->subject; ?></h2>
-  <?php endif; ?>
-  <?php print render($title_suffix); ?>
-  <div class="content"<?php print $content_attributes; ?>>
-    <?php print $content; ?>
+<div id="<?php
+
+print $block_html_id;
+?>" class="<?php
+
+print $classes;
+?>"<?php
+
+print $attributes;
+?>>
+
+  <?php
+
+print render($title_prefix);
+if ($block->subject) {
+  ?>
+  <h2<?php
+
+  print $title_attributes;
+  ?>><?php
+
+  print $block->subject;
+  ?></h2>
+<?php
+
+}
+?>
+  <?php
+
+print render($title_suffix);
+?>
+
+  <div class="content"<?php
+
+print $content_attributes;
+?>>
+    <?php
+
+print $content;
+?>
   </div>
 </div>
